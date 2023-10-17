6475544016e1756b0c04733428aeb353c4c99288
Md Robin Rana <developer.smrobin45@gmail.com>
2023-10-17 12:02:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const router = require("./router/coffee");
+const port = process.env.PORT || 3000;
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// application router
+app.get("/", (req, res) => {
+   res.send("CoffeeServer is running");
+});
+app.use("/coffee", router);
+
+// Server listen
+app.listen(port, () => {
+   console.log(`Server is running on ${port}`);
+});
