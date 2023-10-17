d9b0b63e47352eb61903fc1e91ab91b8a82e1a5b
Md Robin Rana <developer.smrobin45@gmail.com>
2023-10-17 13:29:20 +0600

    first commit

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const { MongoClient } = require("mongodb");
 const port = process.env.PORT || 3000;
 const app = express();
 
+// Cloud uri
+const mongoUri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.ywsqr.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(mongoUri);
+
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // application router
-
-// Cloud uri
-const mongoUri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.ywsqr.mongodb.net/?retryWrites=true&w=majority`;
-
-const client = new MongoClient(mongoUri);
+app.get("/", (req, res) => {
+   res.send("CoffeeServer is running");
+});
+app.use("/coffee", router);
 
 client.connect((err) => {
    if (err) {
@@ -27,8 +31,3 @@ client.connect((err) => {
       console.log(`Server is running on ${port}`);
    });
 });
-
-app.get("/", (req, res) => {
-   res.send("CoffeeServer is running");
-});
-app.use("/coffee", router);
